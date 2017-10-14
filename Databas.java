3e525f04b65df9373998029fc4049cbab66d3923
Jakob <jakob.lindahl@yh.nackademin.se>
2017-10-14 12:02:35 +0200

    finetuning

--- Databas.java
+++ Databas.java
@@ -12,25 +12,25 @@ public class Databas {
 
     private List<IPerson> personer = new ArrayList<>();
 
-    public Databas(){
+    public Databas() {
 
-        try (BufferedReader br = Files.newBufferedReader(customers);){
-            
+        try (BufferedReader br = Files.newBufferedReader(customers);) {
+
             String firstline;
             String secondline;
-            
+
             while ((firstline = br.readLine().trim()) != null && (secondline = br.readLine()) != null) {
-                
+
                 String[] personnummernamn = firstline.split(", ");
                 LocalDate datum = LocalDate.parse(secondline.trim());
-                
+
                 Person person = makePerson(personnummernamn, datum);
                 try {
                     addPerson(person);
                 } catch (NullPointerException e) {
                     System.out.println("Personen " + personnummernamn[1] + " gick inte att skapa och blev ej tillaggd i listan");
                 }
-                
+
             }
         } catch (IOException e) {
             System.out.println("något gick fel");
@@ -42,10 +42,10 @@ public class Databas {
         return personer;
     }
 
-    public void addPerson(Person person)throws NullPointerException{
+    public void addPerson(Person person) throws NullPointerException {
         if (person != null) {
             personer.add(person);
-        }else{
+        } else {
             throw new NullPointerException();
         }
     }
@@ -56,9 +56,9 @@ public class Databas {
         String namn = personnummerNamn[1].trim();
 
         if (datum.isAfter(yearago)) {
-            person = new isKund(personnummmer,namn);
+            person = new isKund(personnummmer, namn);
         } else {
-            person = new beenKund(personnummmer,namn);
+            person = new beenKund(personnummmer, namn);
         }
         return person;
     }
